89bd1b94859f4a043dc0cbba44d6d27d59270661
Avijitbera <avijitbera775@gmail.com>
2025-02-03 23:24:30 +0530

    created SiteBuilder function on frontend

---
+++ frontend/src/pages/SiteBuilder.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+
+const SiteBuilder = () =>{
+    return (
+        <div className="min-h-screen bg-gray-100 p-6">
+
+        </div>
+    )
+}
+
+export default SiteBuilder
